1fcec6af06e5d8d43be5a0a124ae4a74186f827c
victor gabriel <victorgabrielalmeidapradosilva@gmail.com>
2026-07-07 21:58:15 -0300

    antes das novas features

--- app/Views/layouts/guest.php
+++ app/Views/layouts/guest.php
@@ -12,7 +12,7 @@ $appName = config('app.name', 'Fechou');
     <title><?= e($title ?? $appName) ?></title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&family=Newsreader:ital,opsz,wght@0,6..72,400;0,6..72,500;0,6..72,600;1,6..72,400;1,6..72,500&family=IBM+Plex+Mono:wght@400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?= e(asset('css/app.css')) ?>">
 </head>
 <body class="shell shell-auth">
